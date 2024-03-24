516cbd409bc4e797d65c10098989097e83d5d7b9
Phil <philwekullo@gmail.com>
2024-03-24 15:06:08 +0300

    Video carousel section complete.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+function App() {
+
+  return (
+    <main className="bg-black">
+      <Nav/>
+      <Hero/>
+      <Highlights/>
+    </main>
+  )
+}
+
+export default App
