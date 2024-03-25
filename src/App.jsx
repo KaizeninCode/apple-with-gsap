5e0a7f6d3d4bb00c461d0a6166c6243f200ee05a
Phil <philwekullo@gmail.com>
2024-03-26 00:08:50 +0300

    complete website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
+import * as Sentry from '@sentry/react'
 
 function App() {
 
@@ -9,8 +14,12 @@ function App() {
       <Nav/>
       <Hero/>
       <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
