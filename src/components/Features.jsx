8cd78dc7a057a3206ac78451e38516f279431550
Phil Wekullo <149161187+KaizeninCode@users.noreply.github.com>
2024-03-28 03:07:39 +0300

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -32,8 +32,8 @@ const Features = () => {
   }, []);
 
   return (
-    <section className="h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-wdith">
+    <section className="h-full common-padding bg-zinc relative">
+      <div className="screen-max-width">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">Explore the full story.</h1>
         </div>
